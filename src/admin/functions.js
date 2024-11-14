b83fdf1c80786b891f795d51aa92af889365ce8d
Unnat1654 <125393204+unnat1654@users.noreply.github.com>
2024-11-14 11:57:37 +0530

    trying to deploy

--- src/admin/functions.js
+++ src/admin/functions.js
@@ -22,7 +22,6 @@ export const validateAllNextQuestions = (questions, questionNumbers) => {
     throw new Error("Contains no ending for form");
   }
   const uniqueNextQuesArray = Array.from(uniqueNextQuestions);
-  console.log(questionNumbers)
   if (uniqueNextQuesArray.some(NextQues => (!questionNumbers.includes(`${NextQues}`) && NextQues!=""))) {
     throw new Error("Next Questions contains unknown question numbers")
   }
