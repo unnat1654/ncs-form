1eef412e1e0bed8336026ec552cc7f23226892b5
Unnat1654 <125393204+unnat1654@users.noreply.github.com>
2024-11-15 12:10:45 +0530

    added routing to hackncs.in

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "react-toastify/dist/ReactToastify.css"; //toast
 import { Routes, Route } from "react-router-dom";
 import FormCreator from "./admin/FormCreator";
@@ -10,6 +10,15 @@ import Signup from "./user/Signup";
 import "./App.css";
 import "./user/Login.css";
 
+
+const ExternalRedirect = ({ url }) => {
+  useEffect(() => {
+    window.location.href = url;
+  }, [url]);
+
+  return null;
+};
+
 const App = () => {
   return (
     <Routes>
@@ -18,7 +27,7 @@ const App = () => {
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
       <Route path="/form/:id" element={<ChatBot />} />
-      <Route path="*" element={<Signup />} />
+      <Route path="*" element={<ExternalRedirect url="https://hackncs.in" />} />
     </Routes>
   );
 };
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,9 +3,11 @@ import React, { useState } from "react";
 import logo from "../assets/NCS_Logo.svg";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { useAuth } from "../authContext";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [auth,setAuth]=useAuth();
 
   return (
     <div className="navbar">
@@ -14,29 +16,21 @@ const Navbar = () => {
       </div>
       <div className="navbar-right">
         <div className="navbar-links">
-          <a href="/" className="navbar-link">
+          <Link to="https://hackncs.in" className="navbar-link">
             Home
-          </a>
-          <a href="/projects" className="navbar-link">
+          </Link>
+          <Link to="https://hackncs.in/projects" className="navbar-link">
             Projects
-          </a>
-          <a href="/team" className="navbar-link">
+          </Link>
+          <Link to="https://hackncs.in/team" className="navbar-link">
             Team
-          </a>
+          </Link>
           <Link to="/login" className="navbar-link">
             Login
           </Link>
         </div>
-        <button className="connect-btn">Connect</button>
-        {/* Hamburger icon */}
-        <div
-          className={`hamburger ${menuOpen ? "open" : ""}`}
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
-        </div>
+        <Link to="https://hackncs.in/#connect"><button className="connect-btn">Connect</button></Link>
+
       </div>
     </div>
   );
